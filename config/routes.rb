0b47e582cec08b3e2f13e3e4c7bc60003074a79d
Diptiman <diptiman.pati@byjus.com>
2022-08-18 12:07:10 +0530

    Graphiql test in production try-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  if Rails.env.development? || Rails.env.staging?
+  if Rails.env.development? || Rails.env.staging? || Rails.env.production?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
